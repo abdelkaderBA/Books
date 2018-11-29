b995ca73b94d174b9b4148609edb0a67aa31bf25
abdelkaderBA <benaicha.gaddour@gmail.com>
2018-11-29 16:06:39 +0000

    SignIn/SignUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-root',
